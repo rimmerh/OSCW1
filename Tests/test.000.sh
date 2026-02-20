3536d5ff6b83ade373e448fcaf4de51879795c01
rimmerh <harryrimmer23@icloud.com>
2026-02-20 17:03:46 +0000

    new trace00

---
+++ Tests/test.000.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# TEST 0
+# Zero-entanglement control test (based on test 1)
+
+# set simple entangled CPUs (1, 3)
+X=0
+Y=0
+echo $X > /proc/sys/kernel/entangled_cpus_1
+echo $Y > /proc/sys/kernel/entangled_cpus_2
+
+sleep 1
+
+# compile the test program
+gcc -o work work.c
+
+# set up ftrace
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+echo nop > /sys/kernel/debug/tracing/current_tracer
+echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable
+printf "%x\n" "$(( (1 << X) | (1 << Y) ))" > /sys/kernel/debug/tracing/tracing_cpumask
+
+echo 1 > /sys/kernel/debug/tracing/tracing_on
+
+# simple entanglement test
+/bin/su -s /bin/bash -c "taskset -c $X ./work 10" testuser1 &
+/bin/su -s /bin/bash -c "taskset -c $Y ./work 10" testuser2 &
+
+sleep 10
+
+# disable tracing
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+cp /sys/kernel/debug/tracing/trace ./trace.txt
